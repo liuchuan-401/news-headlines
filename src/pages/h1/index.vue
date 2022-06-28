44189cbf5d525491d79b2d154d9e5910e9e2c449
liuchuan-401 <liuchuan401@163.com>
2022-06-28 17:34:01 +0800

    完成启动页

---
+++ src/pages/h1/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div
+    class='start'
+    v-if="isStar"
+  >
+    <img src='../../assets/logo.png'>
+    <h4>新闻头条</h4>
+    <p>有态度的技术分享</p>
+  </div>
+  <div
+    class="h"
+    v-else
+  >
+    <img
+      src="@/assets/Snipaste.png"
+      class='pic'
+    >
+    <h4>新闻<span>头条</span></h4>
+    <p>技术热点的追求者</p>
+    <van-button
+      class="btn"
+      round
+      size="large"
+      to="/login"
+    >立即体验</van-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isStar: true
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.isStar = false
+    }, 2000)
+  }
+
+}
+</script>
+
+<style scoped lang='less'>
+.start {
+  height: 667px;
+  text-align: center;
+  background-color: #3594fa;
+  img {
+    margin: 200px 58px 88px;
+    width: 259px;
+    height: 190px;
+  }
+  h4 {
+    font-size: 30px;
+    color: #fefeff;
+  }
+  p {
+    margin-bottom: 100px;
+    font-size: 24px;
+    color: #fefeff;
+  }
+}
+.h {
+  text-align: center;
+  .pic {
+    margin: 176px 82px 0;
+    width: 218px;
+    height: 174px;
+  }
+  h4 {
+    margin: 87px 0 15px 0;
+    font-size: 25px;
+    span {
+      color: #3698fa;
+    }
+  }
+  p {
+    margin: 0;
+    margin-bottom: 62px;
+    font-size: 20px;
+    color: #666666;
+  }
+  .btn {
+    width: 130px;
+    height: 32px;
+    font-size: 18px;
+    letter-spacing: 1px;
+    color: #3698fa;
+    border-radius: 16px;
+    border: solid 1px #3698fa;
+  }
+}
+</style>
--- src/routers/index.js
+++ src/routers/index.js
@@ -6,9 +6,16 @@ Vue.use(VueRouter)
 
 // 引入组件
 import Login from '@/pages/login/index.vue'
+import H from '@/pages/h1/index.vue'
 
 // 配置路由规则
 const routes = [
+  // 访问页面
+  {
+    path: '/',
+    component: H
+  },
+
   //登录页面
   {
     path: '/login',
